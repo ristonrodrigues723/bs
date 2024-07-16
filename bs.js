581bdcd525b2c15e4e00e52ac7ba90aeb50922cc
Rodrigues Ricky Dinesh <121783748+ristonrodrigues723@users.noreply.github.com>
2024-07-16 23:48:54 +0530

    chamges

--- bs.js
+++ bs.js
@@ -1,7 +1,9 @@
-const array = [2, 5, 8, 12, 16, 23, 38, 56, 72, 91];
+let array = [2, 5, 8, 12, 16, 23, 38, 56, 72, 91];
 const container = document.getElementById('array-container');
 
 function createBoxes() {
+    container.innerHTML = '';
+    array.sort((a, b) => a - b);
     array.forEach(num => {
         const box = document.createElement('div');
         box.className = 'box';
@@ -23,7 +25,17 @@ async function binarySearch(target) {
         boxes.forEach(box => box.classList.remove('selected', 'found', 'not-found'));
         boxes[mid].classList.add('selected');
 
-   
+        await new Promise(resolve => setTimeout(resolve, 1000));
+
+        if (array[mid] === target) {
+            boxes[mid].classList.add('found');
+            return mid;
+        } else if (array[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
 
     document.querySelectorAll('.box').forEach(box => box.classList.add('not-found'));
     return -1;
@@ -32,4 +44,28 @@ async function binarySearch(target) {
 function startSearch() {
     const target = parseInt(document.getElementById('search-input').value);
     binarySearch(target);
+}
+
+function addNumber() {
+    const num = parseInt(document.getElementById('add-input').value);
+    if (!isNaN(num) && !array.includes(num)) {
+        array.push(num);
+        createBoxes();
+    }
+    document.getElementById('add-input').value = '';
+}
+
+function removeNumber() {
+    const num = parseInt(document.getElementById('remove-input').value);
+    const index = array.indexOf(num);
+    if (index > -1) {
+        array.splice(index, 1);
+        createBoxes();
+    }
+    document.getElementById('remove-input').value = '';
+}
+
+function clearArray() {
+    array = [];
+    createBoxes();
 }
